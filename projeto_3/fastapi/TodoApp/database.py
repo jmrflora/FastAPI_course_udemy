f9c8e8b989d76aa289601fc857f35261bf0c03ab
José Matheus R. Flora <jmrfloraxbox@gmail.com>
2023-09-15 11:36:02 -0300

    finalizando section 8

--- projeto_3/fastapi/TodoApp/database.py
+++ projeto_3/fastapi/TodoApp/database.py
@@ -6,7 +6,7 @@ SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
-Sessionlocal = sessionmaker(autocommit= false, autoflush=False, bind=engine)
+Sessionlocal = sessionmaker(autocommit= False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
