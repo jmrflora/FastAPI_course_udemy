149e5a3a85ffae55bd91f986bdb997fd9ded1bce
José Matheus R. Flora <jmrfloraxbox@gmail.com>
2023-09-18 19:46:50 -0300

    quase no fim dessa secao

--- projeto_3/fastapi/TodoApp/routers/auth.py
+++ projeto_3/fastapi/TodoApp/routers/auth.py
@@ -1,7 +1,46 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from database import Sessionlocal
+from models import Users
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from starlette import status
 
 router = APIRouter()
 
-@router.get("/auth/")
-async def get_user():
-    return {'user': 'authenticated'}
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+def get_db():
+    db = Sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
+    
+
+db_dependecy = Annotated[Session, Depends(get_db)]
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependecy,
+                      create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email = create_user_request.email,
+        username = create_user_request.username,
+        first_name = create_user_request.first_name,
+        last_name = create_user_request.last_name,
+        hashed_password = bcrypt_context.hash(create_user_request.password), 
+        is_active =  True,
+        role = create_user_request.role
+    )
+
+    db.add(create_user_model)
+    db.commit()
